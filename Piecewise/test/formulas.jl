85ff58da6c206c961cfb70cb0d1c84e8312f85e4
Christophe Berthod <christophe.berthod@unige.ch>
2025-07-25 15:53:43 +0200

    floats in tests

--- Piecewise/test/formulas.jl
+++ Piecewise/test/formulas.jl
@@ -1,10 +1,10 @@
 
 @testset "POLY" begin
 
-    @test Piecewise.POLY_F(1, [1, 2, 3, 4, 5, 6]) == 21
+    @test Piecewise.POLY_F(1, [1, 2, 3, 4, 5, 6]) ≈ 21
     @test Piecewise.POLY_F([1, 2, 3], 2) == [2, 4, 6]
     @test Piecewise.POLY_F([1, 2, 3, 4]) == [1, -2, 3, -4]
-    @test Piecewise.POLY_F(1, [1, 2, 3, 4, 5, 6], 1) == 4.4071428571428575
+    @test Piecewise.POLY_F(1, [1, 2, 3, 4, 5, 6], 1) ≈ 4.4071428571428575
     @test typeof(POLY) == Formula
 
 end
@@ -12,23 +12,23 @@ end
 
 @testset "TAIL" begin
 
-    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 5]) == 0.25
+    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 5]) ≈ 0.25
     @test_throws ArgumentError Piecewise.TAIL_F([1, 2, -3, 4, 5], (0, 1), (true, true), true, true)
     @test Piecewise.TAIL_F([1, 2, -3, 4, 5], (0, 1), (true, true), true, false) == false
     @test Piecewise.TAIL_F([1, 2, 3, 4, 5], (0, 1), (true, true), true, true) == true
     @test Piecewise.TAIL_F([1, 2, -3, 4, 5], (0, 1), (true, true), true, false) == false
     @test Piecewise.TAIL_F([1, 2, 3, 4, 5], 2) == [2, 4, 3, 4, 5]
     @test Piecewise.TAIL_F([1, 2, 3, 4, 5]) == [1, -2, 3, -4, 5]
-    @test Piecewise.TAIL_F(1, [1, 2, 0, 0, 5], 0) == -0.2
-    @test Piecewise.TAIL_F(1, [1, 2, 0, 0, 5], 1) == 0.4
-    @test Piecewise.TAIL_F(1, [1, 2, 0, 0, 5], 5) == 0.13
-    @test Piecewise.TAIL_F(1, [1, 2, 0, 4, 5], 0) == 0.32958368660043297
-    @test Piecewise.TAIL_F(1, [1, 2, 0, 4, 5], 1) == 0.30268837405404053
-    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 0], 1) == 0.2044341744113004
-    @test Piecewise.TAIL_F(1, [1, 2, 3, 0, 0], 1) == 0.38888888888888884
-    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 0], 1) == 0.2044341744113004
-    @test Piecewise.TAIL_F(1, [1, 2, 1, 2, 1], 1) == 0.4205584583201638
-    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 5], 1) == 0.14729513605358416
+    @test Piecewise.TAIL_F(1, [1, 2, 0, 0, 5], 0) ≈ -0.2
+    @test Piecewise.TAIL_F(1, [1, 2, 0, 0, 5], 1) ≈ 0.4
+    @test Piecewise.TAIL_F(1, [1, 2, 0, 0, 5], 5) ≈ 0.13
+    @test Piecewise.TAIL_F(1, [1, 2, 0, 4, 5], 0) ≈ 0.32958368660043297
+    @test Piecewise.TAIL_F(1, [1, 2, 0, 4, 5], 1) ≈ 0.30268837405404053
+    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 0], 1) ≈ 0.2044341744113004
+    @test Piecewise.TAIL_F(1, [1, 2, 3, 0, 0], 1) ≈ 0.38888888888888884
+    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 0], 1) ≈ 0.2044341744113004
+    @test Piecewise.TAIL_F(1, [1, 2, 1, 2, 1], 1) ≈ 0.4205584583201638
+    @test Piecewise.TAIL_F(1, [1, 2, 3, 4, 5], 1) ≈ 0.14729513605358416
     @test typeof(TAIL) == Formula
 
 end
@@ -36,7 +36,7 @@ end
 
 @testset "LOG" begin
 
-    @test Piecewise.LOG_F(0.5, [1, 1]) == -0.6931471805599453
+    @test Piecewise.LOG_F(0.5, [1, 1]) ≈ -0.6931471805599453
     @test_throws ArgumentError Piecewise.LOG_F([1, 1], (0, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.LOG_F([1, 1], (1, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.LOG_F([1, 1], (0, 1), (true, true), true, true)
@@ -45,8 +45,8 @@ end
     @test Piecewise.LOG_F([1, 1], (0, 1), (true, true), true, false) == false
     @test Piecewise.LOG_F([1, 1], (0, 1), (true, false), true, true) == true
     @test Piecewise.LOG_F([1, 1], (1, 2), (false, true), true, true) == true
-    @test Piecewise.LOG_F(1, [0, 1], 0) == -1.0
-    @test Piecewise.LOG_F(1, [2, 1], 0) == 0.38629436111988963
+    @test Piecewise.LOG_F(1, [0, 1], 0) ≈ -1.0
+    @test Piecewise.LOG_F(1, [2, 1], 0) ≈ 0.38629436111988963
     @test typeof(LOG) == Formula
 
 end
@@ -54,7 +54,7 @@ end
 
 @testset "ISRS" begin
 
-    @test Piecewise.ISRS_F(0.5, [1, 1]) == 1.1547005383792517
+    @test Piecewise.ISRS_F(0.5, [1, 1]) ≈ 1.1547005383792517
     @test_throws ArgumentError Piecewise.ISRS_F([1, 1], (0, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.ISRS_F([1, 1], (1, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.ISRS_F([1, 1], (0, 1), (true, true), true, true)
@@ -63,9 +63,9 @@ end
     @test Piecewise.ISRS_F([1, 1], (0, 1), (true, true), true, false) == false
     @test Piecewise.ISRS_F([1, 1], (0, 1), (true, false), true, true) == true
     @test Piecewise.ISRS_F([1, 1], (1, 2), (false, true), true, true) == true
-    @test Piecewise.ISRS_F(1, [0, 1], 0) == 0.0
-    @test Piecewise.ISRS_F(1, [0, 1], 1) == 1.0
-    @test Piecewise.ISRS_F(1, [2, 1], 0) == 0.5235987755982989
+    @test Piecewise.ISRS_F(1, [0, 1], 0) ≈ 0.0
+    @test Piecewise.ISRS_F(1, [0, 1], 1) ≈ 1.0
+    @test Piecewise.ISRS_F(1, [2, 1], 0) ≈ 0.5235987755982989
     @test typeof(ISRS) == Formula
 
 end
@@ -73,7 +73,7 @@ end
 
 @testset "PLS" begin
 
-    @test Piecewise.PLS_F(0.5, [1, -1/2, 1]) == 1.4142135623730951
+    @test Piecewise.PLS_F(0.5, [1, -1/2, 1]) ≈ 1.4142135623730951
     @test_throws ArgumentError Piecewise.PLS_F([1, -1/2, 1], (0, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.PLS_F([1, -1/2, 1], (1, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.PLS_F([1, -1/2, 1], (0, 1), (true, true), true, true)
@@ -84,9 +84,9 @@ end
     @test Piecewise.PLS_F([2, -13, 1], (0, 1), (true, true), true, false) == false
     @test Piecewise.PLS_F([1, -1/2, 1], (0, 1), (true, false), true, true) == true
     @test Piecewise.PLS_F([1, -1/2, 1], (1, 2), (false, true), true, true) == true
-    @test Piecewise.PLS_F(0.5, [0, -2, 1], 1) == -0.6931471805599453
-    @test Piecewise.PLS_F(0.5, [0, -2, 1], 2) == 0.5
-    @test Piecewise.PLS_F(0.5, [1, -1/2, 1], 2) == 0.05314694696594851
+    @test Piecewise.PLS_F(0.5, [0, -2, 1], 1) ≈ -0.6931471805599453
+    @test Piecewise.PLS_F(0.5, [0, -2, 1], 2) ≈ 0.5
+    @test Piecewise.PLS_F(0.5, [1, -1/2, 1], 2) ≈ 0.05314694696594851
     @test typeof(PLS) == Formula
 
 end
@@ -94,15 +94,15 @@ end
 
 @testset "XLOG" begin
 
-    @test Piecewise.XLOG_F(0.5, [1, 1]) == -0.34657359027997264
+    @test Piecewise.XLOG_F(0.5, [1, 1]) ≈ -0.34657359027997264
     @test_throws ArgumentError Piecewise.XLOG_F([1, 1], (0, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.XLOG_F([1, 1], (1, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.XLOG_F([1, 1], (0, 1), (true, true), true, true)
-    @test Piecewise.XLOG_F(1, [0, 1], 0) == -0.25
-    @test Piecewise.XLOG_F(1, [2, 1], 0) == 0.1362943611198904
+    @test Piecewise.XLOG_F(1, [0, 1], 0) ≈ -0.25
+    @test Piecewise.XLOG_F(1, [2, 1], 0) ≈ 0.1362943611198904
     @test Piecewise.XLOG_F([1, 1]) == [-1, -1]
-    @test Piecewise.XLOG_F(1, [0, 1], 0) == -0.25
-    @test Piecewise.XLOG_F(1, [2, 1], 0) == 0.1362943611198904
+    @test Piecewise.XLOG_F(1, [0, 1], 0) ≈ -0.25
+    @test Piecewise.XLOG_F(1, [2, 1], 0) ≈ 0.1362943611198904
     @test typeof(XLOG) == Formula
 
 end
@@ -110,13 +110,13 @@ end
 
 @testset "XISRS" begin
 
-    @test Piecewise.XISRS_F(0.5, [1, 1]) == 0.5773502691896258
+    @test Piecewise.XISRS_F(0.5, [1, 1]) ≈ 0.5773502691896258
     @test_throws ArgumentError Piecewise.XISRS_F([1, 1], (0, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.XISRS_F([1, 1], (1, 2), (true, true), true, true)
     @test_throws ArgumentError Piecewise.XISRS_F([1, 1], (0, 1), (true, true), true, true)
     @test Piecewise.XISRS_F([1, 1]) == [-1, -1]
-    @test Piecewise.XISRS_F(1, [0, 1], 0) == 1.0
-    @test Piecewise.XISRS_F(1, [2, 1], 0) == 0.2679491924311225
+    @test Piecewise.XISRS_F(1, [0, 1], 0) ≈ 1.0
+    @test Piecewise.XISRS_F(1, [2, 1], 0) ≈ 0.2679491924311225
     @test typeof(XISRS) == Formula
 
 end
--- PiecewiseLorentz/test/runtests.jl
+++ PiecewiseLorentz/test/runtests.jl
@@ -10,7 +10,7 @@ using Test
 
     # Evaluation with negative imaginary part and moment expansion
     @test_throws DomainError LorentzTransform(f, 2)(0, im)
-    @test LorentzTransform(f, 2, 1)(1, 5 - im) == 0.00023287042221692184
+    @test LorentzTransform(f, 2, 1)(1, 5 - im) ≈ 0.00023287042221692184
 
     # Initialization with function lacking primitive
     f = PiecewiseFunction(Piece((0, 1), x -> x))
@@ -36,12 +36,12 @@ end
     Le(p, m) = LorentzTransform(PiecewiseFunction(:even, p), m)
 
     p = Piece((0, 1), POLY, [1, 2])
-    @test Lp(p, 1)(0, -im) == 0.47063560015265193
-    @test Le(p, 1)(0, -im) == 0.9412712003053039
-    @test Lp(p, 2)(0, -im) == 0.11577962350472716
-    @test Le(p, 2)(0, -im) == 0.23155924700945432
-    @test Lp(p, 3)(0, -im) == 0.029656069987218852
-    @test Le(p, 3)(0, -im) == 0.059312139974437704
+    @test Lp(p, 1)(0, -im) ≈ 0.47063560015265193
+    @test Le(p, 1)(0, -im) ≈ 0.9412712003053039
+    @test Lp(p, 2)(0, -im) ≈ 0.11577962350472716
+    @test Le(p, 2)(0, -im) ≈ 0.23155924700945432
+    @test Lp(p, 3)(0, -im) ≈ 0.029656069987218852
+    @test Le(p, 3)(0, -im) ≈ 0.059312139974437704
     @test_throws ArgumentError Lp(p, 4)(0, -im)
     @test_throws ArgumentError Le(p, 4)(0, -im)
 
@@ -50,12 +50,12 @@ end
     @test_throws ArgumentError Le(p, 1)(0, -im)
 
     p = Piece((0, 1), LOG, [2, 1])
-    @test Lp(p, 1)(0, -im) == 0.1066633502558379
-    @test Le(p, 1)(0, -im) == 0.2133267005116758
-    @test Lp(p, 2)(0, -im) == 0.030087400117168876
-    @test Le(p, 2)(0, -im) == 0.06017480023433775
-    @test Lp(p, 3)(0, -im) == 0.008636588104053535
-    @test Le(p, 3)(0, -im) == 0.01727317620810707
+    @test Lp(p, 1)(0, -im) ≈ 0.1066633502558379
+    @test Le(p, 1)(0, -im) ≈ 0.2133267005116758
+    @test Lp(p, 2)(0, -im) ≈ 0.030087400117168876
+    @test Le(p, 2)(0, -im) ≈ 0.06017480023433775
+    @test Lp(p, 3)(0, -im) ≈ 0.008636588104053535
+    @test Le(p, 3)(0, -im) ≈ 0.01727317620810707
     @test_throws ArgumentError Lp(p, 4)(0, -im)
     @test_throws ArgumentError Le(p, 4)(0, -im)
 
@@ -64,22 +64,22 @@ end
     @test_throws ArgumentError Le(p, 1)(0, -im)
 
     p = Piece((0, 1), PLS, [2, -1/2, 1])
-    @test Lp(p, 1)(0, -im) == 0.2028882993824653
-    @test Le(p, 1)(0, -im) == 0.4057765987649306
-    @test Lp(p, 2)(0, -im) == 0.05189177586576684
-    @test Le(p, 2)(0, -im) == 0.10378355173153368
-    @test Lp(p, 3)(0, -im) == 0.013779563659096715
-    @test Le(p, 3)(0, -im) == 0.02755912731819343
+    @test Lp(p, 1)(0, -im) ≈ 0.2028882993824653
+    @test Le(p, 1)(0, -im) ≈ 0.4057765987649306
+    @test Lp(p, 2)(0, -im) ≈ 0.05189177586576684
+    @test Le(p, 2)(0, -im) ≈ 0.10378355173153368
+    @test Lp(p, 3)(0, -im) ≈ 0.013779563659096715
+    @test Le(p, 3)(0, -im) ≈ 0.02755912731819343
     @test_throws ArgumentError Lp(p, 4)(0, -im)
     @test_throws ArgumentError Le(p, 4)(0, -im)
 
     p = Piece((0, 1), XLOG, [2, 1])
-    @test Lp(p, 1)(0, -im) == 0.0338958049472908
-    @test Le(p, 1)(0, -im) == 0.0677916098945816
-    @test Lp(p, 2)(0, -im) == 0.008665178161249475
-    @test Le(p, 2)(0, -im) == 0.01733035632249895
-    @test Lp(p, 3)(0, -im) == 0.002270885882873601
-    @test Le(p, 3)(0, -im) == 0.004541771765747201
+    @test Lp(p, 1)(0, -im) ≈ 0.0338958049472908
+    @test Le(p, 1)(0, -im) ≈ 0.0677916098945816
+    @test Lp(p, 2)(0, -im) ≈ 0.008665178161249475
+    @test Le(p, 2)(0, -im) ≈ 0.01733035632249895
+    @test Lp(p, 3)(0, -im) ≈ 0.002270885882873601
+    @test Le(p, 3)(0, -im) ≈ 0.004541771765747201
     @test_throws ArgumentError Lp(p, 4)(0, -im)
     @test_throws ArgumentError Le(p, 4)(0, -im)
 
